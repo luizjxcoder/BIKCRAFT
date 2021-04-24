5f7a4e9eb7aa4f049892f7ade07d0c726d0206b9
LuizJunior34 <luiz.jun19@gmail.com>
2021-04-23 22:46:38 -0300

    javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-var footer = document.querySelector(".footer_redes")
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+});
+
+new SimpleSlide({
+    slide: 'portfolio', 
+    time: 5000, 
+    nav: true
+  });
+  
+
