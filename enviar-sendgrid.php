d5482e4e8b5ca5f9e0337714dbcff0ecb7b3fdb5
LuizJunior34 <luiz.jun19@gmail.com>
2021-05-14 17:44:40 -0300

    php

---
+++ enviar-sendgrid.php
@@ -0,0 +1,37 @@
+<?php
+
+require("./sendgrid-php/sendgrid-php.php");
+
+$email_site = "contato@bikcraft.com";
+$nome_site = "Bikcraft";
+
+$email_user = $_POST["email"];
+$nome_user = $_POST["nome"];
+
+$body_content = "";
+foreach( $_POST as $field => $value) {
+  if( $field !== "leaveblank" && $field !== "dontchange" && $field !== "enviar") {
+    $sanitize_value = filter_var($value, FILTER_SANITIZE_STRING);
+    $body_content .= "$field: $value \n";
+  }
+}
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom($email_site, $nome_site);
+$email->addTo($email_site, $nome_site);
+
+$email->setReplyTo($email_user, $nome_user);
+
+$email->setSubject("Formulário Bikcraft");
+$email->addContent("text/plain", $body_content);
+
+$sendgrid = new \SendGrid("
+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");
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo "Caught exception: ". $e->getMessage() ."\n";
+}
